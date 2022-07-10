93febebbb0b92760af75e73ee0d60c41d93bc72e
Di Saber <marc.a.benamou@gmail.com>
2022-07-10 08:56:10 -0500

    Removed test code that I didn't notice

--- Password Generator/Program.cs
+++ Password Generator/Program.cs
@@ -14,7 +14,6 @@ internal class Program
         Console.Write("Generator Seed (leave blank for random): ");
 
         var seed = Console.ReadLine();
-        Console.WriteLine(seed);
 
         if (seed != null)
         {
